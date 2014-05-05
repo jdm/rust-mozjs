41d34db9932be8d4e1c2113483cae6441c4f2c67
Josh Matthews <josh@joshmatthews.net>
2014-05-05 12:44:14 -0400

    Regenerate JSAPI bindings for upgraded SpiderMonkey.

--- jsfriendapi.rs
+++ jsfriendapi.rs
@@ -22,12 +22,12 @@ pub fn JS_ObjectToOuterObject(cx: *mut JSContext,
 //pub type JSJitInfo = JSJitInfo_struct;
 
 pub mod bindgen {
-    use jsapi::{JSContext, JSObject, JSClass, JSRuntime};
+    use jsapi::{JSContext, JSObject, JSClass, JSRuntime, JSHandleObject};
     use libc::uintptr_t;
 
     extern {
         pub fn JS_NewObjectWithUniqueType(cx: *mut JSContext, clasp: *JSClass,
-                                          proto: *mut JSObject, parent: *mut JSObject) -> *mut JSObject;
+                                          proto: JSHandleObject, parent: JSHandleObject) -> *mut JSObject;
         pub fn JS_GetAddressableObject(rt: *mut JSRuntime, candidateObj: uintptr_t) -> *mut JSObject;
     }
 }
